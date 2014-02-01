4a0265463a563e1f1147aee2f70d8c2e10a3abaa
Mathew Beane <aepod23@gmail.com>
2014-02-01 00:22:30 -0600

    ARGv parsing looking better...

--- src/shell/categoryGenerator.php
+++ src/shell/categoryGenerator.php
@@ -4,24 +4,46 @@ require_once( dirname( $_SERVER['argv'][0] ) . '/abstract.php' );
 
 class Mage_Shell_Category_Generator extends Mage_Shell_Abstract
 {
+	public $dryrun;
+	public $template;
+	public $rootCategories;
+	public $categoryRecursion;
 
+	public function __construct(){
+		//Setting the defaults
+		$this->dryrun = false;
+		$this->template = "category-base.xml";
+		$this->rootCategories = 1;
+		$this->categoryRecursion = 1;
+	}
     /**
      * Run script
      *
      */
     public function run()
     {
-        $args = $this->_parseArgs();
-        if ($args[0] == "stub") {
-        	print_r($args);
-        } else {
+    	$executedRun = false;   //Check against, instead of using if/else on args
+    	
+        $args = $this->_parseArgs(); // args can be non-linear
+        
+        
+        if ($this->dryrun) {
+        	$executedRun = true;  // Set to true, now help wont trigger.
+        	echo "Dry run initiated.\n";
+        	print_r($this);
+        	
+        	exit;
+        }
+        
+        if(!$executedRun)
+        {
             echo $this->usageHelp();
-
+            print_r($args);
         }
     }
 
     /**
-     * Get the real cleaned argv
+     * Parse through the args, build an array and parse in any details.
      *
      * @return array
      */
@@ -31,6 +53,28 @@ class Mage_Shell_Category_Generator extends Mage_Shell_Abstract
     					create_function( '$e',
     							'return $e != \'--\';' ) ),
     			1 );
+    	
+    	// Bring settings in - There has to be a cleaner way of doing this!
+    	foreach($args as $_arg){
+    		//strip the leading --
+    		if(substr($_arg,0,2) == "--") $_arg = substr($_arg,2);
+    		
+    		// $_arg[0] is the "command"
+    		// $_arg[1] is the "value"
+    		$_arg = explode("=",$_arg);
+    		
+    		if($_arg[0] == "-n"){
+    			$this->dryrun = true;
+    		}
+    		
+    		//bring in settings against existing class properties if they exist
+    		if(key_exists($_arg[0],$this)){
+    			$this->$_arg[0] = $_arg[1];
+    		}
+  			
+    	}
+
+    	
     	return $args;
     }    
     
@@ -43,7 +87,7 @@ class Mage_Shell_Category_Generator extends Mage_Shell_Abstract
         return <<<USAGE
 Usage:  php -f categoryGenerator.php -- [options]
 
-  stub                    Example
+  -n                Make this not really happen
   help                    This help
 
 USAGE;
